968056ba5f94cad9e7fd7dc85d90fecc942689d2
S-M Lee <leessung97@daum.net>
2020-04-13 20:54:11 +0900

    HighSchoolAllTime

--- app/src/main/java/com/example/highschoolalltime/MainActivity.java
+++ app/src/main/java/com/example/highschoolalltime/MainActivity.java
@@ -4,24 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.app.Person;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btn4, btn5;
+    Button btn1, btn2, btn3, btn4, btn5, btn_add;
+    TextView tv_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button)findViewById(R.id.btn_1);
-        btn2 = (Button)findViewById(R.id.btn_2);
-        btn3 = (Button)findViewById(R.id.btn_3);
-        btn4 = (Button)findViewById(R.id.btn_4);
-        btn5 = (Button)findViewById(R.id.btn_5);
+        btn1 = (Button) findViewById(R.id.btn_1);
+        btn2 = (Button) findViewById(R.id.btn_2);
+        btn3 = (Button) findViewById(R.id.btn_3);
+        btn4 = (Button) findViewById(R.id.btn_4);
+        btn5 = (Button) findViewById(R.id.btn_5);
+        btn_add = (Button) findViewById(R.id.btn_add);
+        tv_result = (TextView) findViewById(R.id.tv_result_m1);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +81,39 @@ public class MainActivity extends AppCompatActivity {
                 transaction.commit();
             }
         });
+        btn_add.setOnClickListener(new View.OnClickListener() {
 
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setIcon(R.mipmap.ic_launcher);
+                ad.setTitle("과목추가");
+                ad.setMessage("과목명");
+
+                final EditText et = new EditText(MainActivity.this);
+                ad.setView(et);
+                ad.setMessage("수업 교시");
+
+                final EditText et2 = new EditText(MainActivity.this);
+                ad.setView(et2);
+
+                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String subject = et.getText().toString();
+                        String result = et2.getText().toString();
+                        dialog.dismiss();
+                    }
+                });
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                ad.show();
+            }
+        });
     }
 }
