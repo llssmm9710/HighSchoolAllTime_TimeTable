6d80b412e1ed23f20817461da42591900449a8d2
S-M Lee <leessung97@daum.net>
2020-05-06 17:29:32 +0900

    HighSchoolAllTime_TimeTable

--- app/src/main/java/com/example/highschoolalltime/MainActivity.java
+++ app/src/main/java/com/example/highschoolalltime/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.highschoolalltime;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,107 +14,25 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btn4, btn5, btn_add;
-    TextView tv_result, tv_result2;
+    private Button btn_hom, btn_timetable, btn_cafeteria, btn_bullentin_board, btn_my_page;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button) findViewById(R.id.btn_1);
-        btn2 = (Button) findViewById(R.id.btn_2);
-        btn3 = (Button) findViewById(R.id.btn_3);
-        btn4 = (Button) findViewById(R.id.btn_4);
-        btn5 = (Button) findViewById(R.id.btn_5);
-        btn_add = (Button) findViewById(R.id.btn_add);
-        tv_result = (TextView) findViewById(R.id.tv_result_m1);
-        tv_result2 = (TextView) findViewById(R.id.tv_result_m2);
+        btn_hom = findViewById(R.id.btn_1);
+        btn_timetable = findViewById(R.id.btn_2);
+        btn_cafeteria = findViewById(R.id.btn_3);
+        btn_bullentin_board = findViewById(R.id.btn_4);
+        btn_my_page = findViewById(R.id.btn_5);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+
+        btn_timetable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment1 fragment1 = new Fragment1();
-                transaction.replace(R.id.frame, fragment1);
-                transaction.commit();
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment2 fragment2 = new Fragment2();
-                transaction.replace(R.id.frame, fragment2);
-                transaction.commit();
-            }
-        });
-
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment3 fragment3 = new Fragment3();
-                transaction.replace(R.id.frame, fragment3);
-                transaction.commit();
-            }
-        });
-
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment4 fragment4 = new Fragment4();
-                transaction.replace(R.id.frame, fragment4);
-                transaction.commit();
-            }
-        });
-
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                Fragment5 fragment5 = new Fragment5();
-                transaction.replace(R.id.frame, fragment5);
-                transaction.commit();
-            }
-        });
-        btn_add.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
-                ad.setIcon(R.mipmap.ic_launcher);
-                ad.setTitle("과목추가");
-                ad.setMessage("과목명");
-
-                final EditText et = new EditText(MainActivity.this);
-                ad.setView(et);
-
-                ad.setMessage("수업 교시");
-
-                final EditText et2 = new EditText(MainActivity.this);
-                ad.setView(et2);
-
-                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String subject = et.getText().toString();
-                        String result = et2.getText().toString();
-                        tv_result.setText(result);
-                        tv_result2.setText(subject);
-                        dialog.dismiss();
-                    }
-                });
-                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-                ad.show();
+                Intent intent = new Intent(MainActivity.this, TimeTableActivity.class);
+                startActivity(intent);
             }
         });
     }
