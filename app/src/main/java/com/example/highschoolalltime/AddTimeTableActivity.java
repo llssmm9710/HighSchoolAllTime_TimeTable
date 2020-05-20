f2dca6a356b2e7065c7d5431a1d78067a21db6cb
S-M Lee <leessung97@daum.net>
2020-05-20 18:49:51 +0900

    HighSchoolAllTime_TimeTable<10th>

--- app/src/main/java/com/example/highschoolalltime/AddTimeTableActivity.java
+++ app/src/main/java/com/example/highschoolalltime/AddTimeTableActivity.java
@@ -2,33 +2,22 @@ package com.example.highschoolalltime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Array;
-import java.sql.Time;
-
 public class AddTimeTableActivity extends AppCompatActivity {
 
-    private Button add;
-    String result[] = {"m", "tu", "w", "t", "f"};
-    String[] re_day, re_time;
-    String sub, text;
-    TextView tv_result;
+    private Button add;//추가버튼
+    String result[] = {"m", "tu", "w", "t", "f"};//String을 이용해 데이터 저장 할 위치값 찾을 시 추가할 문자열
+    String[] re_day, re_time;//Spiner값을 비교할 배열
+    String sub, text;//TextView ID값과 입력한 데이터를 갖을 임시 변수
+    TextView tv_result;//Edit Text로 입력 받은 데이터를 삽입할 TextView
 
 
     @Override
@@ -36,45 +25,45 @@ public class AddTimeTableActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_table);
 
-        add = findViewById(R.id.btn_add);
-        re_day = getResources().getStringArray(R.array.day);
-        re_time = getResources().getStringArray(R.array.time);
+        add = findViewById(R.id.btn_add);//버튼 아이디 값 불러오기
+        re_day = getResources().getStringArray(R.array.day);//Spiner의 값을 비교할 변수 불러오기
+        re_time = getResources().getStringArray(R.array.time);//Spiner의 값을 비교할 변수 불러오기
 
-        add.setOnClickListener(new View.OnClickListener() {
+        add.setOnClickListener(new View.OnClickListener() {//버튼 클릭 이벤트
             @Override
             public void onClick(View v) {
-                show();
+                show();//데이터 입력 화면으로 전환하는 함수
             }
         });
     }
     void show(){
-        re_day = getResources().getStringArray(R.array.day);
-        re_time = getResources().getStringArray(R.array.time);
+        //re_day = getResources().getStringArray(R.array.day);
+        //re_time = getResources().getStringArray(R.array.time);
 
-        AlertDialog.Builder ad = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
+        AlertDialog.Builder ad = new AlertDialog.Builder(this);//데이터를 입력받을 화면 다이얼로그로 띄우기 위한 Builder
+        LayoutInflater inflater = getLayoutInflater();//다이얼로그 내 띄울 Layout 불러오기
         View view = inflater.inflate(R.layout.dialog_add, null);
-        ad.setView(view);
-        final Button submit = (Button) view.findViewById(R.id.buttonSubmit);
-        final Button done = (Button) view.findViewById(R.id.buttonDone);
-        final EditText subject = (EditText) view.findViewById(R.id.edittextSubject);
-        final Spinner day = (Spinner) view.findViewById(R.id.edittextDay);
-        final Spinner time = (Spinner) view.findViewById(R.id.edittextTime);
+        ad.setView(view);//불러온 Layout을 다이얼로그에 띄우기
+        final Button submit = (Button) view.findViewById(R.id.buttonSubmit);//다이얼로그 내 버튼 ID정의(Posivive<확인> 버튼)
+        final Button done = (Button) view.findViewById(R.id.buttonDone);//다이얼로그 내 버튼 ID정의(Negative<취소> 버튼)
+        final EditText subject = (EditText) view.findViewById(R.id.edittextSubject);//과목명을 입력받을 Edit Text ID 정의
+        final Spinner day = (Spinner) view.findViewById(R.id.edittextDay);//요일 입력받을 Spiner ID 정의
+        final Spinner time = (Spinner) view.findViewById(R.id.edittextTime);//수업시간 입력받을 Spiner ID 정의
 
-        final AlertDialog dialog = ad.create();
-        submit.setOnClickListener(new View.OnClickListener() {
+        final AlertDialog dialog = ad.create();//다이얼로그 생성
+        submit.setOnClickListener(new View.OnClickListener() {//Positive(확인) 버튼
             @Override
             public void onClick(View v) {
-                String sub_text = subject.getText().toString();
+                String sub_text = subject.getText().toString();//Edit Text 임시 저장 변수
 
-                sub = "tv_result_";
+                sub = "tv_result_";//TextView ID값을 String으로 비교하기 위한 임시 문자열 변수 초기화
 
-                text = day.getSelectedItem().toString();
-                for(int i = 0; i < re_day.length; i++){if(text.equals(re_day[i])){sub = sub + result[i];}}
-                text = time.getSelectedItem().toString();
-                for(int i = 0; i < re_time.length; i++){if(text.equals(re_time[i])){sub = sub + re_time[i];}}
+                text = day.getSelectedItem().toString();//Sriner로 입력받은 요일 임시 저장
+                for(int i = 0; i < re_day.length; i++){if(text.equals(re_day[i])){sub = sub + result[i];}}//for문을 이용해 반복적으로 임시 저장한 요일 값을 요일 배열과 비교해 참인 경우 초기화 했던 문자열 뒤에 추가
+                text = time.getSelectedItem().toString();//Sriner로 입력받은 수강시간 임시 저장
+                for(int i = 0; i < re_time.length; i++){if(text.equals(re_time[i])){sub = sub + (i+1);}}//for문을 이용해 반복적으로 임시 저장한 수강시간 값을 요일 배열과 비교해 참인 경우 문자열 뒤에 추가
 
-                switch (sub){
+                switch (sub){//Switch문을 이용해 문자열과 Layout 내 TextView ID 값들과 비교해 TextView ID값을 정의, <m:월, t:화, w:수, tu:목, f:금>, 문자열 제일 뒤는 수강 시간으로 ID값을 지정
                     case "tv_result_m1":
                         tv_result = findViewById(R.id.tv_result_m1); break;
                     case "tv_result_m2":
@@ -150,17 +139,17 @@ public class AddTimeTableActivity extends AppCompatActivity {
                     case "tv_result_f7":
                         tv_result = findViewById(R.id.tv_result_f7); break;
                 }
-                tv_result.setText(sub_text);
+                tv_result.setText(sub_text);//Switch문으로 지정한 TextView에 입력받은 과목명을 삽입
 
-                dialog.dismiss();
+                dialog.dismiss();//다이얼로그 종료
             }
         });
-        done.setOnClickListener(new View.OnClickListener() {
+        done.setOnClickListener(new View.OnClickListener() {//Negative<취소> 버튼
             @Override
             public void onClick(View v) {
-                dialog.dismiss();
+                dialog.dismiss();//<취소> 버튼을 클릭 시 값을 입력 받았음에도 아무런 동작 없이 다이얼로그 종료
             }
         });
-        dialog.show();
+        dialog.show();//다이얼로그 화면에 띄우는 함수
     }
 }
--- app/src/main/java/com/example/highschoolalltime/MainActivity.java
+++ app/src/main/java/com/example/highschoolalltime/MainActivity.java
@@ -16,18 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_hom = findViewById(R.id.btn_1);
-        btn_timetable = findViewById(R.id.btn_2);
-        btn_cafeteria = findViewById(R.id.btn_3);
-        btn_bullentin_board = findViewById(R.id.btn_4);
-        btn_my_page = findViewById(R.id.btn_5);
+        btn_hom = findViewById(R.id.btn_1);//어플 메인 화면으로  이동 버튼
+        btn_timetable = findViewById(R.id.btn_2);//시간표 화면으로  이동 버튼
+        btn_cafeteria = findViewById(R.id.btn_3);//급식표 화면으로  이동 버튼
+        btn_bullentin_board = findViewById(R.id.btn_4);//게시판 화면으로 이동 버튼
+        btn_my_page = findViewById(R.id.btn_5);//My Page 화면으로 이동 버튼
 
 
         btn_timetable.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddTimeTableActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {//시간표 화면 담당자로 버튼2번만 구현
+                Intent intent = new Intent(MainActivity.this, AddTimeTableActivity.class);//시간표 화면을 구성할 Activity를 따로 생성하였으며 시간표 화면으로 이동하기 위한 버튼을 클릭 시 화면을 전환하기 위해 이동할 Activity를 정의
+                startActivity(intent);//정의한 Activity 시작
             }
         });
     }
